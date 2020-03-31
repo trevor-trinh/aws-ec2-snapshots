c9e59424afbb02793d201c4adebb1b2db6bf53a1
trevzt <trinhtrevor@gmail.com>
2020-03-31 16:38:16 -0700

    Added click module for future features and script now shows more information about ec2 instances

--- snapbot/snapbot.py
+++ snapbot/snapbot.py
@@ -1,9 +1,13 @@
 import boto3
+import click
 
-if __name__ == '__main__':
-    session = boto3.Session(profile_name='snapshotty')
-    ec2 = session.resource('ec2')
+session = boto3.Session(profile_name='snapshotty')
+ec2 = session.resource('ec2')
 
+@click.command()
+def list_instances():    
     for i in ec2.instances.all():
         print(i)
-        
+    
+if __name__ == '__main__':
+    list_instances()
